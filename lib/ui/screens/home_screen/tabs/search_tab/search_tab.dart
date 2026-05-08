bb76d2abac458cc5f9e772658a2e4ea30bb72b08
fariio <fares.ahmed7474@gmail.com>
2026-05-08 21:11:21 +0300

    + Create Search Tab Screen

---
+++ lib/ui/screens/home_screen/tabs/search_tab/search_tab.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:movie_app_graduation_project_route/core/utils/app_assets.dart';
+import 'package:movie_app_graduation_project_route/ui/screens/auth/login_screen/widgets/custom_text_field.dart';
+
+import '../../home.dart';
+
+class SearchTab extends StatelessWidget{
+  List<String> moviePosters = [
+    AppAssets.movie1917,
+    AppAssets.docStrange,
+    AppAssets.blackWidow,
+    AppAssets.captain,
+    AppAssets.ironMan,
+    AppAssets.darkKnight,
+  ];
+
+  SearchTab({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 20,
+        left: 16,
+        right: 16,
+      ),
+      child: Column(
+        children: [
+          CustomTextFormField(
+            prefixIcon: SvgPicture.asset(
+              AppAssets.searchIcon,
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(15),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.7,
+              ),
+              itemCount: moviePosters.length,
+              itemBuilder: (context, index) {
+                return MoviePoster(imagePath: moviePosters[index]);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  
+  
+}
